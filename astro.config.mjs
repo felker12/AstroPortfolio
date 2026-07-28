f15ccc04ced3855153e196beac8b0913bd7056c2
felker12 <kanthonyfelker@yahoo.com>
2026-07-28 19:04:47 -0400

    added sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anthonyfelker.com',
-  integrations: [icon()], 
+  integrations: [icon(), sitemap()], 
   vite: {
     plugins: [tailwindcss()]
   }
